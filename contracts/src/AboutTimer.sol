d16e6d0a06b73d84c9318529bf060b922cab0408
rplusq <rafaplusq@gmail.com>
2023-05-13 14:24:49 +0100

    start the AboutTimer contract

---
+++ contracts/src/AboutTimer.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {
+    ISuperfluid, 
+    ISuperToken, 
+    ISuperApp
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+
+struct Task {
+    address buyer;
+    address seller;
+    uint96 flowRate;
+}
+
+/**
+@title AboutTimer
+@author The It's About Time team
+@notice Allows for the creation of timers that will be calculated based on block timestamps for the start and end event
+@dev User needs to own zkBobx for interacting with this contract
+@dev User needs to setMaxFlowPermissions in zkBobx for this contract
+ */
+contract AboutTimer {
+    event CreatedTask(address indexed buyer, address indexed seller, uint96 flowRate);
+    event FinalizedTask(address indexed buyer, address indexed seller, uint96 flowRate);
+
+    /// @notice CFA Library.
+    using SuperTokenV1Library for ISuperToken;
+    using ECDSA for bytes32;
+
+
+    ISuperToken public zkBobx;
+
+
+    constructor(ISuperToken zkBobx_) {
+        zkBobx = zkBobx_;
+    }
+
+    mapping(address => Task) public sellerTask;
+
+    // function upsertSellerProfile(address seller, int96 flowRate) external {
+    //     sellersFlowrate[seller] = flowRate;
+    // }
+
+    // Tasks 2. Create a timer
+    function startTimer(address buyer, bytes calldata buyerSignature, uint256 orderId, uint96 flowRate) external {
+        // verify buyer signature for message 'I agree to order ${orderId} from ${msg.sender} for ${flowRate} and acknolewdge I have read the terms and conditions of It's About Time!'
+        require(sellerTask[msg.sender].buyer == address(0), "Seller already has a task");
+        require(verifySignature(buyer, flowRate, buyerSignature), "Invalid buyer signature");
+        sellerTask[msg.sender] = Task(buyer, msg.sender, flowRate);
+        emit CreatedTask(buyer, msg.sender, flowRate);
+        zkBobx.createFlowFrom(buyer, msg.sender, flowRate);
+    }
+
+    // Tasks 3. End a timer
+    function endTimer() external {
+        Task memory task = sellerTask[msg.sender];
+        require(task.buyer != address(0), "Seller does not have a task");
+        delete sellerTask[msg.sender];
+        emit FinalizedTask(task.buyer, msg.sender, task.flowRate);
+        // end time is block.timestamp
+        zkBobx.deleteFlow(task.buyer, msg.sender);
+    }
+
+    function verifySignature(
+        address signer,
+        uint256 flowRate,
+        bytes memory signature
+    ) public view returns (bool) {
+        // Construct the message
+        bytes32 message = keccak256(abi.encodePacked(
+            "I agree to order from ",
+            msg.sender,
+            " for ",
+            flowRate,
+            " and acknowledge I have read the terms and conditions of It's About Time!"
+        ));
+
+        // Create the prefixed hash
+        bytes32 prefixedHash = message.toEthSignedMessageHash();
+
+        // Recover the address from the signature
+        address recoveredAddress = prefixedHash.recover(signature);
+
+        // Compare the recovered address with the signer's address
+        return (recoveredAddress == signer);
+    }
+}
--- wagmi.config.ts
+++ wagmi.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
   plugins: [
     foundry({
       deployments: {
-        Counter: {
-          [chains.mainnet.id]: "0x1A61839Eb5fC6eBBcAe01eD5E79062E598792Dac",
-          [chains.goerli.id]: "0x78991BB1D194C1235fe285240af8489CFA552151",
-          [chains.foundry.id]: "0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167",
-        },
+        // Counter: {
+        //   [chains.mainnet.id]: "0x1A61839Eb5fC6eBBcAe01eD5E79062E598792Dac",
+        //   [chains.goerli.id]: "0x78991BB1D194C1235fe285240af8489CFA552151",
+        //   [chains.foundry.id]: "0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167",
+        // },
       },
       project: "./contracts",
     }),
